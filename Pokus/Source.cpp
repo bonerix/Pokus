90b3cf1b1b38d6b94e93c718760000a28718f3db
Bonerix <bonerixyt@gmail.com>
2022-05-16 17:22:57 +0200

    Přidejte soubory projektu.

---
+++ Pokus/Source.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+#include <fstream>
+
+int main()
+{
+    std::fstream file;
+    file.open("C:\\Users\\pajam\\Desktop\\random.nesahat", std::ios_base::out);
+    file.close();
+}
